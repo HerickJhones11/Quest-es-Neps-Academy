902ab4e59bd4c50d380f54c47aaf0fa8b4feb4b2
Herick <59341296+HerickJhones11@users.noreply.github.com>
2022-08-06 15:19:55 -0300

    Create algoritmosordenacao.cpp

---
+++ algoritmosordenacao.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int v[5] = {4, 1, 2, 5, 3};
+    int ordenado = 0;
+    while(ordenado==0){
+        ordenado = 1;
+        for(int i = 0; i < 4; i++){
+            if(v[i] > v[i + 1]){
+                ordenado = 0;
+                int temp = v[i];
+                v[i] = v[i + 1];
+                v[i + 1] = temp;
+            }
+        }
+    }
+    for(int i = 0; i < 5; i++){
+        cout << v[i] << endl;
+    }
+    int v2[] = {4, 1, 2, 5, 3};
+    sort(v2 , v2 + 5);
+    for(int i = 0; i < 5; i++){
+        cout << v2[i] << endl;
+    }
+}
